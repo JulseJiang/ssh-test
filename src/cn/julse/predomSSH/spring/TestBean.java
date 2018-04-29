781c67673cc54e9c71826a354b917867ec098e70
Jules <2213974854@qq.com>
2018-04-29 14:13:46 +0800

    springbean配置

--- src/cn/julse/predomSSH/spring/TestBean.java
+++ src/cn/julse/predomSSH/spring/TestBean.java
@@ -1,13 +1,22 @@
 package cn.julse.predomSSH.spring;
 
+import cn.julse.predomSSH.db.user.Users;
+
 public class TestBean {
     private String name;
-
+    private Users user;
     public void printData(){
         System.out.println("testBean "+name);
+        System.out.println("user "+user);
     }
 
+    public Users getUser() {
+        return user;
+    }
 
+    public void setUser(Users user) {
+        this.user = user;
+    }
 
     public String getName() {
         return name;
--- src/cn/julse/predomSSH/spring/TestBeanTest.java
+++ src/cn/julse/predomSSH/spring/TestBeanTest.java
@@ -1,11 +1,20 @@
 package cn.julse.predomSSH.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
 * creat_user: julse@qq.com creat_date: 2018/4/26
 **/
 public class TestBeanTest {
     public static void main(String[] args){
-        TestBean testBean = new TestBean();
+        /*TestBean testBean = new TestBean();
         testBean.setName("xiaoming");
+        testBean.printData();*/
+        //创建共享区
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //取出共享区中的对象
+        TestBean testBean = (TestBean) applicationContext.getBean("testBean");
         testBean.printData();
     }
 }
